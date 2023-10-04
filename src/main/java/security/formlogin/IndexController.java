287353c757180634491e4c2a9e551151a65cea55
ljn425 <ljn425@naver.com>
2023-10-04 16:53:32 +0900

    권할설정

--- src/main/java/security/formlogin/IndexController.java
+++ src/main/java/security/formlogin/IndexController.java
@@ -1,7 +1,12 @@
 package security.formlogin;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class IndexController {
@@ -11,8 +16,30 @@ public class IndexController {
         return "index";
     }
 
-    @GetMapping("/loginPage")
-    public String loginPage() {
-        return "loginPage";
+    @GetMapping("/user")
+    public String user() {
+        return "user";
     }
+    @GetMapping("/admin/pay")
+    public String adminPay() {
+        return "adminPay";
+    }
+    @GetMapping("/admin/**")
+    public String adminAll() {
+        return "adminAll";
+    }
+
+    @GetMapping("/token")
+    public Map<String, String> getCsrfToken(HttpServletRequest request) {
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        String token = csrfToken.getToken();
+        String headerName = csrfToken.getHeaderName();
+        Map<String, String> map = new HashMap<>();
+        map.put("token", token);
+        map.put("headerName", headerName);
+        System.out.println(map.toString());
+        return map;
+    }
+
+
 }
--- src/main/java/security/formlogin/SecurityConfig.java
+++ src/main/java/security/formlogin/SecurityConfig.java
@@ -6,7 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,24 +18,37 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public UserDetailsManager users() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1111") // {noop}은 암호화를 하지 않는다는 의미
+                .roles("USER")
+                .build();
+        UserDetails sys = User.builder()
+                .username("sys")
+                .password("{noop}1111")
+                .roles("SYS")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1111")
+                .roles("ADMIN", "SYS", "USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, sys, admin);
+    }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated()
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                    .requestMatchers("/user").hasRole("USER") // user 요청에 대해 USER 권한만 허용
+                    .requestMatchers("/admin/pay").hasRole("SYS") // admin/pay 요청에 대해 SYS 권한만 허용
+                    .requestMatchers("/admin/**").hasAnyRole("ADMIN", "SYS") // admin/** 요청에 대해 ADMIN, SYS 권한만 허용
+                    .requestMatchers("/token").permitAll() // token 요청에 대해 모든 권한 허용
+                    .anyRequest().authenticated() // 나머지 요청들은 인증되었을 시 자원 허용
                 )
                 .formLogin(withDefaults())
-                .sessionManagement(sessionManagement -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // 세션 정책
-                        .sessionFixation().changeSessionId() // 세션 고정 보호(default), none: 세션 고정 공격에 당함, migrateSession, newSession
-                        .maximumSessions(1) // 최대 허용 가능 세션 수, -1은 무제한 로그인 세션 허용
-                        .maxSessionsPreventsLogin(false) // true 동시 로그인 차단(기존 세션 유지), false는 기존 세션 만료(default)
-                        .expiredUrl("/expired") // 세션이 만료된 경우 이동할 페이지
-//                                .sessionRegistry(sessionRegistry -> {}) // 세션 저장소 설정
-//                                .expiredSessionStrategy((expiredSessionStrategy) -> {}) // 세션 만료 후 처리
-                );
-
-        return http.build();
+                .build();
     }
 }
