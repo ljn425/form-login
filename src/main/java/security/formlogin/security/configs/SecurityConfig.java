709a72391f8503b17b9f3bc1e15634ce63ae5a89
ljn425 <ljn425@naver.com>
2023-10-04 17:47:31 +0900

    인증 프로세스 form 인증 구현- 프로젝트 기본 설정

---
+++ src/main/java/security/formlogin/security/configs/SecurityConfig.java
@@ -0,0 +1,64 @@
+package security.formlogin.security.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/mypage").hasRole("USER")
+                        .requestMatchers("/messages").hasRole("MANAGER")
+                        .requestMatchers("/config").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager() {
+
+        String password = passwordEncoder().encode("1111");
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(password)
+                .roles("USER")
+                .build();
+
+        UserDetails manager = User.builder()
+                .username("manager")
+                .password(password)
+                .roles("MANAGER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(password)
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, manager, admin);
+    }
+}
