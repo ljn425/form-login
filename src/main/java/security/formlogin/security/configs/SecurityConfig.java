4e73d9851abafcf60cf44893f9b0998bae78d19d
ljn425 <ljn425@naver.com>
2023-10-06 14:48:04 +0900

    정적 자원 관리

--- src/main/java/security/formlogin/security/configs/SecurityConfig.java
+++ src/main/java/security/formlogin/security/configs/SecurityConfig.java
@@ -1,11 +1,14 @@
 package security.formlogin.security.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -22,6 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // 정적 리소스 허용
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/mypage").hasRole("USER")
                         .requestMatchers("/messages").hasRole("MANAGER")
@@ -50,15 +54,25 @@ public class SecurityConfig {
         UserDetails manager = User.builder()
                 .username("manager")
                 .password(password)
-                .roles("MANAGER")
+                .roles("MANAGER", "USER")
                 .build();
 
         UserDetails admin = User.builder()
                 .username("admin")
                 .password(password)
-                .roles("ADMIN")
+                .roles("ADMIN", "MANAGER", "USER")
                 .build();
 
         return new InMemoryUserDetailsManager(user, manager, admin);
     }
+
+//    스프링 시큐리티에서 web.ignoring은 권장하지 않음 대신 seucurityFilterchain permitAll()로 처리한다.
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) ->
+//                // 정적 자원(resources/static)에 대한 요청은 인증/인가 처리를 하지 않음
+//                web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//    }
+
+
 }
